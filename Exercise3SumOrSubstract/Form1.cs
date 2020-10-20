f84f32b5ab56c25887c3180d41d555e33a71dfc7
BahDiallo Mamadou <thiernomamadousaidoubah@paucasesnovescifp.cat>
2020-10-21 00:16:08 +0200

    This is the exercise 4a when we were asked to do the same operation as the last exercise but in this one we gotta use a button to select he operand

---
+++ Exercise3SumOrSubstract/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise3SumOrSubstract
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = "+";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = "-";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int variable1 = Convert.ToInt32(textBox1.Text);
+            int variable2 = Convert.ToInt32(textBox2.Text);
+            int resultado;
+            String variable = textBox4.Text;
+            if (variable == "+")
+            {
+                resultado = variable1 + variable2;
+            }
+            else
+            {
+                resultado = variable1 - variable2;
+            }
+            textBox3.Text = resultado.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
