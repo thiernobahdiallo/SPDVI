077e53d8c1085ecb82030b7f4737c57a58be08eb
BahDiallo Mamadou <thiernomamadousaidoubah@paucasesnovescifp.cat>
2020-10-21 00:08:02 +0200

    This is the 3rd exercise and its about a simple form to sum two numbers using 3 textboxs

---
+++ Exercise3Sum2numbers/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise3Sum2numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int variable1 = Convert.ToInt32(textBox1.Text);
+            int variable2 = Convert.ToInt32(textBox2.Text);
+            int resultado = variable1 + variable2;
+            textBox3.Text = resultado.ToString();
+        }
+    }
+}
