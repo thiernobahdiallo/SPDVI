a8d1fed6c3ac50b978fe99f2c1d39b7435e9eb82
BahDiallo Mamadou <thiernomamadousaidoubah@paucasesnovescifp.cat>
2020-10-21 00:05:52 +0200

    Secon exercise with a chekbox that says when its cheked and when its uncheked

---
+++ Exercice2Forms/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercice2Forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                MessageBox.Show("Checkbox checked");
+            }
+            else
+                MessageBox.Show("Checkbox unchecked");
+        }
+    }
+}
