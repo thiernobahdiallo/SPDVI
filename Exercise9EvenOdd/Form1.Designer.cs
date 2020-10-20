f7466394ac568d1f271b7fc45cd8c94a1d4bf767
BahDiallo Mamadou <thiernomamadousaidoubah@paucasesnovescifp.cat>
2020-10-21 00:30:15 +0200

    This is the exercise 9 where we did a form with a combobox and two buttons: one that when clikced insert even numbers into the combobox and another doing the same but with odd numbers and when one number of the combobox cliked to be copied into a textbox

---
+++ Exercise9EvenOdd/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Exercise9EvenOdd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEven = new System.Windows.Forms.Button();
+            this.buttonOdd = new System.Windows.Forms.Button();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.comboBoxNumbers = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // buttonEven
+            // 
+            this.buttonEven.Location = new System.Drawing.Point(418, 65);
+            this.buttonEven.Name = "buttonEven";
+            this.buttonEven.Size = new System.Drawing.Size(75, 23);
+            this.buttonEven.TabIndex = 0;
+            this.buttonEven.Text = "Even (par)";
+            this.buttonEven.UseVisualStyleBackColor = true;
+            this.buttonEven.Click += new System.EventHandler(this.buttonEven_Click);
+            // 
+            // buttonOdd
+            // 
+            this.buttonOdd.Location = new System.Drawing.Point(576, 65);
+            this.buttonOdd.Name = "buttonOdd";
+            this.buttonOdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonOdd.TabIndex = 1;
+            this.buttonOdd.Text = "Odd (impar)";
+            this.buttonOdd.UseVisualStyleBackColor = true;
+            this.buttonOdd.Click += new System.EventHandler(this.buttonOdd_Click);
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(242, 274);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(409, 20);
+            this.textBoxResult.TabIndex = 3;
+            // 
+            // comboBoxNumbers
+            // 
+            this.comboBoxNumbers.FormattingEnabled = true;
+            this.comboBoxNumbers.Location = new System.Drawing.Point(127, 100);
+            this.comboBoxNumbers.Name = "comboBoxNumbers";
+            this.comboBoxNumbers.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxNumbers.TabIndex = 5;
+            this.comboBoxNumbers.SelectedIndexChanged += new System.EventHandler(this.comboBoxNumbers_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxNumbers);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.buttonOdd);
+            this.Controls.Add(this.buttonEven);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEven;
+        private System.Windows.Forms.Button buttonOdd;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.ComboBox comboBoxNumbers;
+    }
+}
+
---
+++ Exercise9EvenOdd/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise9EvenOdd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEven_Click(object sender, EventArgs e)
+        {
+            comboBoxNumbers.Items.Clear();
+            for(int i = 2; i < 12; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    comboBoxNumbers.Items.Add(i);
+                }
+            }   
+        }
+
+        private void buttonOdd_Click(object sender, EventArgs e)
+        {
+            comboBoxNumbers.Items.Clear();
+            for (int i = 1; i < 11; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    comboBoxNumbers.Items.Add(i);
+                }
+            }
+        }
+
+        private void comboBoxNumbers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = comboBoxNumbers.SelectedIndex;
+            String numero = "";
+            numero = numero + comboBoxNumbers.Items[index];
+            textBoxResult.Text = numero;
+        }
+    }
+}
